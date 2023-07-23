266ab269cf4129eefd18c2144e7ccabc21b8602a
shahafSegal <goigadol360@gmail.com>
2023-07-23 16:34:51 +0300

    have the name changing to title

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const pageTitle = document.getElementById('pageTitle');
 $(function() {
     $('#common-header').load('header.html');
+
+    var pageTitle = document.title;
+    $('#header-paragraph').text(pageTitle);
 });
