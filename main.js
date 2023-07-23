a5badddf50691f2401e041728bdc41c5e6e3915e
shahafSegal <goigadol360@gmail.com>
2023-07-23 17:17:24 +0300

    added footer in js instead of infile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 $(function() {
+    /* div with id=common-header change to be footer.html , uses title from document*/
     $.get('header.html', function(data) {
         $('#common-header').html(data);
         
         // Dynamically update the paragraph based on the page title
         var pageTitle = document.title;
-        $('#header-paragraph').text('Welcome to ' + pageTitle + '!');
+        $('#header-paragraph').text(pageTitle);
     });
+
+    /* div with id=common-footer change to be footer.html*/
+    $.get('footer.html', function(data) {
+        $('#common-footer').html(data);
+    });
+    
 });
